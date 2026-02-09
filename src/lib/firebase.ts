689e652ccb90c62b983fa981e7a47083ee78dc1b
watttab <wattanapong.t64@gmail.com>
2026-02-09 16:17:07 +0700

    Update Firebase config with correct project ID and database URL

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAxKFUdk5jKrZB4OQb2ALo5cea7q0Tvxu0",
-    authDomain: "kosumponngam2.firebaseapp.com",
-    databaseURL: "https://kosumponngam2-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "kosumponngam2",
-    storageBucket: "kosumponngam2.appspot.com",
-    messagingSenderId: "528441609923",
-    appId: "1:528441609923:web:b81bf5870618c0885b1c76",
-    measurementId: "G-T840L9322B"
+    apiKey: "AIzaSyAgCfDxoGGwwms8FQ-qOdF0BUcBQkevK6w",
+    authDomain: "login-c7923.firebaseapp.com",
+    projectId: "login-c7923",
+    storageBucket: "login-c7923.firebasestorage.app",
+    messagingSenderId: "423447261233",
+    appId: "1:423447261233:web:724fbcbbb5d59743df2510",
+    measurementId: "G-V1G3PB4BXQ",
+    databaseURL: "https://login-c7923-default-rtdb.asia-southeast1.firebasedatabase.app"
 };
 
 const app = initializeApp(firebaseConfig);
