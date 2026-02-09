fb05139900a1e02a99b8c5606c441a0a0cc48725
watttab <wattanapong.t64@gmail.com>
2026-02-09 13:31:56 +0700

    Configure for GitHub Pages: HashRouter and homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
